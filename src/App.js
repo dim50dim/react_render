2212b08ecf062283feb7bed42c40dcd2a1276c7d
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-15 19:50:18 +0200

    added MovingDot component to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React from 'react';
 import { useState } from 'react';
+import MovingDot from './MovingDot';
 
 
   function App() {
     return (
       <>
-        
+        <MovingDot />
       </>
     )
   }
