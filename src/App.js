c3f5cb94f7300f08ec05dff2ce6bbd3644e8f9fe
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-15 09:52:38 +0200

    added component Clock to App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Image from './Image';
 import Gallery from './Gallery';
+import Clock from './Clock';
 
 function App() {
   return (
    <>
      <Gallery/>
+     <Clock />
    </>
   );
 }
