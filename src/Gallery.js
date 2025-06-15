c754939d0005c1e75d7cf4e20fbdcfa23ecf242d
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-15 09:49:59 +0200

    added section tag and h1 .

--- src/Gallery.js
+++ src/Gallery.js
@@ -3,9 +3,13 @@ import Image from "./Image";
 export default function Gallery() {
     return (
         <>
+              <section>
+                  <h1>Inspiring Sculptures</h1>
+
            <Image/>
            <Image/>
            <Image/>
+              </section>
         </>
     )
 }
