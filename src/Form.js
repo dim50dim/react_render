1e20103e404d73875f3f445d82944d6f6cfa5615
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-16 08:07:45 +0200

    added new object with spread operator.

--- src/Form.js
+++ src/Form.js
@@ -8,5 +8,69 @@ export default function Form () {
                 city: 'Hamburg',
                 image: 'https://i.imgur.com/Sd1AgUOm.jpg',
             }
-          })
+          });
+          function handleNameChange(e) {
+            setPerson({
+                ...person,
+                name: e.target.value
+            });
+          }
+
+          function handleTitleChange(e) {
+            setPerson({
+                ...person,
+                artwork: {
+                     ...person.artwork,
+                     title: e.target.value
+                }
+            })
+          }
+
+          function handleCityChange(e) {
+            setPerson({
+                ...person,
+                artwork: {
+                    ...person.artwork,
+                    city : e.target.value
+                }
+            });
+          }
+
+          function handleImageChange(e) {
+            setPerson({
+                ...person,
+                artwork: {
+                    ...person.artwork,
+                    image: e.target.value
+                }
+            })
+          }
+          return (
+            <>
+                <label>
+                    Name:
+                    <input value={person.name} onChange={handleNameChange} />
+                </label>
+                <label>
+                      Title: 
+                      <input value={person.title} onChange={handleTitleChange} />
+                </label>
+                <label>
+                    City: 
+                    <input value={person.city} onChange={handleCityChange} />
+                </label>
+                <label>
+                    Image: 
+                    <input value={person.image} onChange={handleImageChange} />
+                </label>
+                <p>
+                    <i>{person.artwork.title}</i>
+                    {' by '}
+                    {person.name}
+                    <br/>
+                    (located in {person.artwork.city})
+                </p>
+                <img src={person.artwork.image} alt={person.artwork.title} />
+            </>
+          )
 }
